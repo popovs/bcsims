ca46a99e67707087773bae86701d86f7baee3c33
popovs <sarah.popov@gmail.com>
2026-05-22 14:28:13 -0700

    Add license blurb

--- tests/testthat/test-user.R
+++ tests/testthat/test-user.R
@@ -1,7 +1,18 @@
-# TESTS TO WRITE
+# Copyright 2026 Province of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# Test that the fxn returns system_user_id, user_identifier,
-# identity_source, role_names, email, display_name
+
 with_mock_dir("api", {
   test_that("get_sims_user returns expected fields", {
     expect_contains(names(get_sims_user()), "system_user_id")
